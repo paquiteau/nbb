e29bfd059bf5dd5e707436f44ead5467d4965f94
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-12-07 23:16:08 +0100

    add loading of configuration file.

--- nbb/cli.py
+++ nbb/cli.py
@@ -1,18 +1,58 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 
-from backend import get_formatted_response
+try:
+    import tomllib as toml
+except ImportError:
+    import tomli as toml
+
+from nbb.backend import get_formatted_response
+
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", default=None)
+    parser.add_argument("--simple", action="store_true")
+    parser.add_argument("--compact", action="store_true")
+    parser.add_argument("--verbose", action="store_true")
+    parser.add_argument("stop_name", nargs="*", default=None)
+    ns = parser.parse_args()
+    if ns.verbose:
+        print("This is nbb cli\nVerbose = ON")
+    return ns
+
+
+def _load_conf(filename):
+    with open(filename, "rb") as f:
+        try:
+            conf = toml.load(f)
+        except FileNotFoundError:
+            print(f"{ns.config} specified but not found.")
+            exit(1)
+        except toml.TOMLDecodeError as e:
+            print(e)
+            exit(1)
+    return conf
+
+
+def load_config(ns):
+    """Load config file."""
+    print("loading config ... ")
+
+    if ns.config is None:
+        return _load_conf(os.path.join(os.path.dirname(__file__), "nbb_conf.toml"))
+    return _load_conf(ns.config)
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("stop_name")
-    parser.add_argument("--simple", action="store_true")
-    parser.add_argument("--compact", action="store_true")
-    ns = parser.parse_args()
+    ns = parser()
+    conf = load_config(ns)
 
-    print(get_formatted_response(ns.stop_name, pretty=not ns.simple, compact=ns.compact))
+    print(
+        get_formatted_response(ns.stop_name, pretty=not ns.simple, compact=ns.compact)
+    )
 
 
 if __name__ == "__main__":
